e8988e3d8eb23064cc718ac1653ed76ada58f33c
NarutoYxY117 <narutoyxy.69@gmail.com>
2022-05-24 13:20:23 +0100

    updated

--- src/Components/Tasks.jsx
+++ src/Components/Tasks.jsx
@@ -4,11 +4,12 @@ const Tasks= ({t,del,done}) =>{
 
     return(
         <div className="todo">
-           <div className="name" style={{color:t.status?"green":"black"}}>
+            <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css"></link>
+           <div className="name" style={{color:t.status?"green":"white"}}>
                {t.name}
            </div>
-           <button onClick={() => del(t.name)}>delete</button>
-           <button onClick={() => done(t.name)}>Done</button>
+           <button onClick={() => del(t.name)}className="delete"><i class="fa fa-close"></i></button>
+           <button onClick={() => done(t.name)}className="done"><i class="fa fa-check"></i></button>
         </div>
         
     )
